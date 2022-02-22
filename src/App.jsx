e9d137c872a4565d6b38fccd172b1e62db3733fe
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-02-22 10:12:45 +0530

    22-02

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import Shop_Page from "./components/Shop_Page/Shop_Page";
+
+
+function App() {
+  return (
+    <div >
+        <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
