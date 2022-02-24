401e1b268e7cd922c7bff861f2a4146ecc27f12b
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-02-24 23:58:20 +0530

    loginPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./Components/Home/JSX/Home"
 import { Shop } from './Components/Shop/Shop';
+import { LoginPage } from './Components/Login/LoginPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path='/shop' element={<Shop></Shop>}> </Route>
+        <Route path='/login' element={<LoginPage></LoginPage>}></Route>
       </Routes>
     </div>
   )
---
+++ src/Components/Login/LoginPage.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+import "./LoginPage.css"
+
+export const LoginPage = () => {
+  return (
+    <div id='loginPage'>
+
+        <div className='blueBox'>
+
+        </div>
+
+        <div></div>
+
+    </div>
+  )
+}
